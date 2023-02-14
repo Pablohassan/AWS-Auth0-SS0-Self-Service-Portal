b41a1d7227b04864d1556288496f737badf1ccf0
Gabriel Malet <gabrielmalet@gmail.com>
2023-02-14 18:59:57 +0100

    [Integration] Chore : integration rusmir code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import {useContext} from 'react';
+<<<<<<< HEAD
 import {Auth0Provider} from '@auth0/auth0-react';
 import {Loading} from '@nextui-org/react';
 import Navbar from './components/Navbar';
@@ -47,5 +48,33 @@ export default function App() {
     <Loading size="lg" css={{display: 'flex', mt: 150}}>
       Waiting for configuration...
     </Loading>
+=======
+import {useAuth0} from '@auth0/auth0-react';
+import Navbar from './components/Navbar';
+import CredentialsProvider from './providers/CredentialsProvider';
+import LoginPage from './pages/LoginPage';
+import RouterProviderSelf from './providers/RouterProviderSelf';
+import AwsProvider, {AccountContext, RegionContext, RoleContext} from './providers/AwsProvider';
+
+export default function App() {
+  const {isAuthenticated} = useAuth0();
+  const [account] = useContext(AccountContext);
+  const [region] = useContext(RegionContext);
+  const [role] = useContext(RoleContext);
+
+  return isAuthenticated ? (
+    <AwsProvider>
+      <CredentialsProvider>
+        {credentials => (
+          <>
+            <Navbar account={account} region={region} role={role} credentials={credentials.credentials} />
+            <RouterProviderSelf account={account} region={region} role={role} credentials={credentials.credentials} />
+          </>
+        )}
+      </CredentialsProvider>
+    </AwsProvider>
+  ) : (
+    <LoginPage />
+>>>>>>> 3d7419c ([Integration] Chore : integration rusmir code)
   );
 }
---
+++ src/assets/roles.js
@@ -0,0 +1,17 @@
+const defaultRole = {
+  Id: 'galilee_roles',
+  name: 'galilee_roles'
+};
+
+const roles = [
+  {
+    Id: 'galilee_roles',
+    name: 'galilee_roles'
+  },
+  {
+    Id: 'galilee_god',
+    name: 'galilee_god'
+  }
+];
+
+export default {defaultRole, roles};
