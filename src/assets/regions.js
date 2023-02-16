895c3f13a1bc64615ecd4e1d96ac1d22afb8aa43
Pablohassan <rusmir-sadikovic_student2022@wilder.school>
2023-02-16 15:15:21 +0100

    [SSP] Chore : Fixed linter errors

--- src/assets/regions.js
+++ src/assets/regions.js
@@ -1,81 +1,81 @@
 const defaultRegion = {
-  Id: "eu-west-3",
-  name: "EU (Paris)",
+  Id: 'eu-west-3',
+  name: 'EU (Paris)'
 };
 
 const regions = [
   {
-    Id: "us-east-1",
-    name: "US East (N. Virginia)",
+    Id: 'us-east-1',
+    name: 'US East (N. Virginia)'
   },
 
   {
-    Id: "us-east-2",
-    name: "US East (Ohio)",
+    Id: 'us-east-2',
+    name: 'US East (Ohio)'
   },
   {
-    Id: "us-west-1",
-    name: "US West (N. California)",
+    Id: 'us-west-1',
+    name: 'US West (N. California)'
   },
   {
-    Id: "us-west-2",
-    name: "US West (Oregon)",
+    Id: 'us-west-2',
+    name: 'US West (Oregon)'
   },
   {
-    Id: "ca-central-1",
-    name: "Canada (Central)",
+    Id: 'ca-central-1',
+    name: 'Canada (Central)'
   },
   {
-    Id: "eu-west-1",
-    name: "EU (Ireland)",
+    Id: 'eu-west-1',
+    name: 'EU (Ireland)'
   },
   {
-    Id: "eu-central-1",
-    name: "EU (Frankfurt)",
+    Id: 'eu-central-1',
+    name: 'EU (Frankfurt)'
   },
   {
-    Id: "eu-west-2",
-    name: "EU (London)",
+    Id: 'eu-west-2',
+    name: 'EU (London)'
   },
   {
-    Id: "eu-west-3",
-    name: "EU (Paris)",
+    Id: 'eu-west-3',
+    name: 'EU (Paris)'
   },
   {
-    Id: "eu-north-1",
-    name: "EU (Stockholm)",
+    Id: 'eu-north-1',
+    name: 'EU (Stockholm)'
   },
   {
-    Id: "ap-northeast-1",
-    name: "Asia Pacific (Tokyo)",
+    Id: 'ap-northeast-1',
+    name: 'Asia Pacific (Tokyo)'
   },
   {
-    Id: "ap-northeast-2",
-    name: "Asia Pacific (Seoul)",
+    Id: 'ap-northeast-2',
+    name: 'Asia Pacific (Seoul)'
   },
   {
-    Id: "ap-southeast-1",
-    name: "Asia Pacific (Singapore)",
+    Id: 'ap-southeast-1',
+    name: 'Asia Pacific (Singapore)'
   },
   {
-    Id: "ap-southeast-2",
-    name: "Asia Pacific (Sydney)",
+    Id: 'ap-southeast-2',
+    name: 'Asia Pacific (Sydney)'
   },
   {
-    Id: "ap-south-1",
-    name: "Asia Pacific (Mumbai)",
+    Id: 'ap-south-1',
+    name: 'Asia Pacific (Mumbai)'
   },
   {
-    Id: "sa-east-1",
-    name: "South America (São Paulo)",
+    Id: 'sa-east-1',
+    name: 'South America (São Paulo)'
   },
   {
-    Id: "us-gov-west-1",
-    name: "US Gov West 1",
+    Id: 'us-gov-west-1',
+    name: 'US Gov West 1'
   },
   {
-    Id: "us-gov-east-1",
-    name: "US Gov East 1",
-  },
+    Id: 'us-gov-east-1',
+    name: 'US Gov East 1'
+  }
 ];
-export default { defaultRegion, regions };
+export default {defaultRegion, regions};
