dd5bf547cde5b6a7044fee602589e21346694efa
Pablohassan <rusmir-sadikovic_student2022@wilder.school>
2023-02-23 12:28:58 +0100

    [SSP] Chore : 0.0.2 Fix conf.json and add prettier , change size of  buttons

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,17 +6,23 @@ module.exports = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ddcd51f ([SSP] Chore : 0.0.2 Fix conf.json and add prettier , change size of  buttons)
   endOfLine: 'lf',
 =======
 >>>>>>> 06a0e2e ([SSP] Chore : Fixed linter errors)
 =======
   endOfLine: 'lf',
 >>>>>>> 14032dd ([SSP] Chore : 0.0.2 Fix conf.json and add prettier , change size of  buttons)
+<<<<<<< HEAD
 =======
   endOfLine: 'lf',
 =======
 >>>>>>> 06a0e2e ([SSP] Chore : Fixed linter errors)
 >>>>>>> fa02cf2 ([SSP] Chore : Fixed linter errors)
+=======
+>>>>>>> ddcd51f ([SSP] Chore : 0.0.2 Fix conf.json and add prettier , change size of  buttons)
   singleQuote: true,
   jsxSingleQuote: false,
   trailingComma: 'none',
